811c0ffd1d113c1f38759db7b8edf5c80a2f0878
Yakitori <moshou1219@gmail.com>
2024-04-22 19:41:47 +0800

    Basic Function

---
+++ Assets/Scripts/GeneralButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralButton : MonoBehaviour
+{
+
+
+    public void GeneralOnClick(GameObject[] objToClose, GameObject[] objToOpen)
+    {
+        foreach (GameObject obj in objToClose) obj.SetActive(false);
+
+        foreach (GameObject obj in objToOpen) obj.SetActive(true);
+
+    }
+}

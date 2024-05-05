1f42db8f9bc8b820ed2ad92cc8890f3fa343ca1d
Bugthief <51325045+Bugthief@users.noreply.github.com>
2024-05-05 18:03:52 +0800

    【主场景的设置按钮 暂停菜单】以及一些UI逻辑

---
+++ Project_Eat/Assets/PauseManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public string sceneName_MainMenu;
+
+    void Start()
+    {
+        // 初始化: 隐藏暂停菜单
+        pauseMenu.SetActive(false);
+    }
+
+
+    public void Settings()
+    {
+        //显示暂停的设置菜单
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        // 从暂停菜单返回游戏
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenu()
+    {
+        // 返回开始菜单
+        SceneManager.LoadScene(sceneName_MainMenu);
+    }
+}
--- Project_Eat/Assets/Scripts/StartMenu/SceneManage.cs
+++ Project_Eat/Assets/Scripts/StartMenu/SceneManage.cs
@@ -9,6 +9,13 @@ public class SceneManage : MonoBehaviour
     public string sceneName_Game;
     public GameObject main;
     public GameObject characterSelect;
+    void Start()
+    {
+        // 初始化：隐藏角色选择，显示主菜单
+        main.SetActive(true);
+        characterSelect.SetActive(false);
+        Time.timeScale = 1f;
+    }
 
     public void SwitchtoGameScene()
     {
